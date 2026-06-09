cca225d3b17bdd1306916a824116e4e8d7ef2018
Nathan Bissett <nbissett@uwo.ca>
2026-06-09 01:48:59 -0400

    Got shaker loading screen set up

--- ios/Tippsy/Tippsy/TippsyApp.swift
+++ ios/Tippsy/Tippsy/TippsyApp.swift
@@ -11,14 +11,26 @@ import SwiftUI
 struct TippsyApp: App {
     @StateObject var userViewModel = UserViewModel()
     @State private var isLoggedIn = TokenStore.read() != nil
+    @State private var isLoading = true
 
     var body: some Scene {
         WindowGroup {
             Group {
-                if isLoggedIn {
+                if isLoading {
+                    LoadingView()
+                        .transition(.opacity)
+                        .task {
+                            try? await Task.sleep(for: .seconds(2))
+                            withAnimation(.easeInOut(duration: 0.45)) {
+                                isLoading = false
+                            }
+                        }
+                } else if isLoggedIn {
                     MainTabView(isLoggedIn: $isLoggedIn, viewModel: userViewModel)
+                        .transition(.opacity)
                 } else {
                     LoginOrRegisterView(isLoggedIn: $isLoggedIn)
+                        .transition(.opacity)
                 }
             }
             .onReceive(NotificationCenter.default.publisher(for: .didReceiveUnauthorized)) { _ in
